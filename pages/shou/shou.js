e1945403c3a073f09f5907a14a72f68676ae93a8
Your Name <you@example.com>
2019-07-31 19:02:32 +0800

    mm

--- pages/shou/shou.js
+++ pages/shou/shou.js
@@ -1,3 +1,18 @@
 Page({
-  
+  data: {},
+  onLoad: function () {
+
+  },
+  //打开透明层
+  showRule: function () {
+    this.setData({
+      isRuleTrue: true
+    })
+  },
+  //关闭透明层
+  hideRule: function () {
+    this.setData({
+      isRuleTrue: false
+    })
+  },
 })
