5a0eb270b35bbdbb628d14c35b63117b30eb5a33
Your Name <you@example.com>
2019-07-31 21:14:21 +0800

    mm

--- pages/shou/shou.js
+++ pages/shou/shou.js
@@ -15,4 +15,15 @@ Page({
       isRuleTrue: false
     })
   },
+  menuTap: function (e) {
+    var current = e.currentTarget.dataset.current;//获取到绑定的数据
+    //改变menuTapCurrent的值为当前选中的menu所绑定的数据
+    this.setData({
+      menuTapCurrent: current
+    });
+
+
+  },
 })
+
+
